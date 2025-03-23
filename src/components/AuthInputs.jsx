9291bcd0e42e68d2da0b858c460e5138e5ba667d
Rem Simiyu <remsimiyu@gmail.com>
2025-03-23 11:15:59 +0300

    ReactArt

--- src/components/AuthInputs.jsx
+++ src/components/AuthInputs.jsx
@@ -27,7 +27,7 @@ export default function AuthInputs() {
           <label>Email</label>
           <input
             type="email"
-            className={emailNotValid ? 'invalid' : undefined}
+            // className={emailNotValid ? 'invalid' : undefined}
             onChange={(event) => handleInputChange('email', event.target.value)}
           />
         </p>
@@ -35,7 +35,7 @@ export default function AuthInputs() {
           <label>Password</label>
           <input
             type="password"
-            className={passwordNotValid ? 'invalid' : undefined}
+            // className={passwordNotValid ? 'invalid' : undefined}
             onChange={(event) =>
               handleInputChange('password', event.target.value)
             }
